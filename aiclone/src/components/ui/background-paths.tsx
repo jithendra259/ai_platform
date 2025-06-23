8a6cd8865d1ba15d9d153b3b48eb1c4c76a6da00
jithendra259 <jithendrasubramanyam218@gmail.com>
2025-06-23 14:46:34 +0530

    bg change of herosection

--- aiclone/src/components/ui/background-paths.tsx
+++ aiclone/src/components/ui/background-paths.tsx
@@ -13,7 +13,7 @@ function FloatingPaths({ position }: { position: number }) {
         } ${343 - i * 6}C${616 - i * 5 * position} ${470 - i * 6} ${
             684 - i * 5 * position
         } ${875 - i * 6} ${684 - i * 5 * position} ${875 - i * 6}`,
-        color: `rgba(200, 200, 200, ${0.1 + i * 0.03})`,
+        color: `rgba(15,23,42,${0.1 + i * 0.03})`,
         width: 0.5 + i * 0.03,
     }));
 
@@ -24,7 +24,7 @@ function FloatingPaths({ position }: { position: number }) {
                 viewBox="0 0 696 316"
                 fill="none"
             >
-                <title></title>
+                <title>The only AI tool you need!</title>
                 
                 {paths.map((path) => (
                     <motion.path
@@ -59,7 +59,7 @@ export function BackgroundPaths({
     const words = title.split(" ");
 
     return (
-        <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden bg-gray-900 text-white">
+        <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden bg-white dark:bg-neutral-950">
             <div className="absolute inset-0">
                 <FloatingPaths position={1} />
                 <FloatingPaths position={-1} />
