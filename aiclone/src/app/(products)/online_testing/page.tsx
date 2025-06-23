6e38e4c51ac596ea8b2b24b6471713ea360a5cc2
Ritik-19-12 <sotwalritik617@gma>
2025-06-23 16:35:38 +0530

    final changes in online-testing & procut

--- aiclone/src/app/(products)/online_testing/page.tsx
+++ aiclone/src/app/(products)/online_testing/page.tsx
@@ -35,18 +35,7 @@ export default function ONLINE_TESTING() {
       <div className=" px-4 md:px-20 lg:px-32 py-10  mx-auto max-w-[96rem] ">
         <h1 data-aos="fade-right" data-aos-duration="2000" className="text-4xl md:text-5xl text-black dark:text-white text-center font-bold mb-15">ONLINE_TESTING</h1>
         <section data-aos="fade-down" data-aos-duration="3000" data-aos-delay="100" className="text-center dark:text-white  text-black mb-12 ">
-          <div className="flex flex-row md:flex-row items-center mb-3  ">
-            <div className=" text-2xl text-blue-500"><a href="/">Products</a></div>
-            <div> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#eeeee" fill="none">
-              <path
-                d="M9.00005 6C9.00005 6 15 10.4189 15 12C15 13.5812 9 18 9 18"
-                stroke="currentColor"
-                strokeWidth="1.5"
-              />
-            </svg>
-            </div>
-            <div className=" text-2xl text-green-500">Online Testing</div>
-          </div>
+        
           <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-8">
             <div className="md:w-1/2 space-y-20 text-left">
               <h2 className="text-2xl md:text-3xl font-semibold mb-4">Secure Online Testing Platform: Ensuring Integrity and Convenience</h2>
@@ -61,7 +50,7 @@ export default function ONLINE_TESTING() {
               alt="online"
               width={600}
               height={400}
-              className="w-full md:w-1/2 h-auto rounded-4xl object-contain"
+              className="w-full md:w-1/2 hidden md:block rounded-4xl object-contain"
             />
 
           </div>
@@ -72,28 +61,30 @@ export default function ONLINE_TESTING() {
           <p className="split-text text-2xl  text-gray-700 dark:text-gray-400">Ask anything, generate questions using AI, understand your documents easily, and much more.</p>
         </section>
 
+      
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 text-white">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 text-white px-4 md:px-10 lg:px-20">
+
           {/* Card 1 */}
-          <div data-aos="fade-up-right" data-aos-duration="2000"
-            className="bg-[#fffff] dark:text-white text-black p-6 rounded-4xl leading-tight transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl">
-            <div className="flex flex-col md:flex-row items-center gap-6">
+          <div data-aos="fade-up-right" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl overflow-hidden">
+            <div className="flex flex-col xl:flex-row items-center gap-6">
               <Image src="/image/Online_testing/webcam security.webp" alt="Webcam Security" width={200} height={150}
-                className="w-full md:w-1/2 max-w-xs rounded-4xl object-contain" />
-              <div>
+                className="w-full md:w-1/2 max-w-xs rounded-3xl object-contain" />
+              <div className="text-center md:text-left w-full">
                 <h3 className="text-3xl font-semibold mb-2">Webcam Security</h3>
-                <p className="split-text text-xl">Begin by ensuring the integrity of your tests with built-in webcam monitoring, effectively deterring cheating attempts.</p>
+                <p className="split-text text-xl leading-tight">Begin by ensuring the integrity of your tests with built-in webcam monitoring, effectively deterring cheating attempts.</p>
               </div>
             </div>
           </div>
 
           {/* Card 2 */}
-          <div data-aos="fade-up-left" data-aos-duration="2000"
-            className="bg-[#fffff] dark:text-white  text-black p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl">
-            <div className="flex flex-col-reverse md:flex-row items-center gap-6">
-              <div>
+          <div data-aos="fade-up-left" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl overflow-hidden">
+            <div className="flex flex-col-reverse xl:flex-row items-center gap-6">
+              <div className="text-center md:text-left w-full">
                 <h3 className="text-3xl font-semibold mb-2">Tab Locking</h3>
-                <p className="split-text text-xl">Maintain focus during tests by preventing users from accessing other tabs or applications.</p>
+                <p className="split-text text-xl leading-tight">Maintain focus during tests by preventing users from accessing other tabs or applications.</p>
               </div>
               <Image src="/image/Online_testing/tabalert.webp" alt="Tab Locking" width={200} height={150}
                 className="w-full md:w-1/2 max-w-xs rounded-4xl object-contain" />
@@ -101,25 +92,25 @@ export default function ONLINE_TESTING() {
           </div>
 
           {/* Card 3 */}
-          <div data-aos="fade-up-right" data-aos-duration="2000"
-            className="bg-[#fffff] dark:text-white  text-black p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-xl">
-            <div className="flex flex-col md:flex-row items-center gap-6">
+          <div data-aos="fade-up-right" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl overflow-hidden">
+            <div className="flex flex-col xl:flex-row items-center gap-6">
               <Image src="/image/Online_testing/tab-alert.webp" alt="Tab Alert" width={200} height={150}
                 className="w-full md:w-1/2 max-w-xs rounded-4xl object-contain" />
-              <div>
+              <div className="text-center md:text-left w-full">
                 <h3 className="text-3xl font-semibold mb-2">Tab Alert</h3>
-                <p className="split-text text-xl">Stay informed with real-time alerts, notifying of unauthorized tab switching.</p>
+                <p className="split-text text-xl leading-tight">Stay informed with real-time alerts, notifying of unauthorized tab switching.</p>
               </div>
             </div>
           </div>
 
           {/* Card 4 */}
-          <div data-aos="fade-up-left" data-aos-duration="2000"
-            className="bg-[#fffff] dark:text-white  text-black p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl">
-            <div className="flex flex-col-reverse md:flex-row items-center gap-6">
-              <div>
+          <div data-aos="fade-up-left" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl overflow-hidden">
+            <div className="flex flex-col-reverse xl:flex-row items-center gap-6">
+              <div className="text-center md:text-left w-full">
                 <h3 className="text-3xl font-semibold mb-2">Right-Click Disable</h3>
-                <p className="split-text text-xl">Prevent users from copying or pasting information by disabling right-click functionality.</p>
+                <p className="split-text text-xl leading-tight">Prevent users from copying or pasting information by disabling right-click functionality.</p>
               </div>
               <Image src="/image/Online_testing/disble right click.webp" alt="Right Click Disable" width={200} height={150}
                 className="w-full md:w-1/2 max-w-xs rounded-4xl object-contain" />
@@ -127,40 +118,40 @@ export default function ONLINE_TESTING() {
           </div>
 
           {/* Card 5 */}
-          <div data-aos="fade-up-right" data-aos-duration="2000"
-            className="bg-[#fffff] dark:text-white  text-black p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl">
-            <div className="flex flex-col md:flex-row items-center gap-6">
+          <div data-aos="fade-up-right" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl overflow-hidden">
+            <div className="flex flex-col xl:flex-row items-center gap-6">
               <Image src="/image/Online_testing/time limits.webp" alt="Time Limits" width={200} height={150}
                 className="w-full md:w-1/2 max-w-xs rounded-4xl object-contain" />
-              <div>
+              <div className="text-center md:text-left w-full">
                 <h3 className="text-3xl font-semibold mb-2">Time Limits</h3>
-                <p className="split-text text-xl">Set time limits to simulate real-world conditions and encourage timely completion.</p>
+                <p className="split-text text-xl leading-tight">Set time limits to simulate real-world conditions and encourage timely completion.</p>
               </div>
             </div>
           </div>
 
           {/* Card 6 */}
-          <div data-aos="fade-up-left" data-aos-duration="2000"
-            className="bg-[#fffff] dark:text-white  text-black p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl">
-            <div className="flex flex-col md:flex-row items-center gap-6">
-              <div>
+          <div data-aos="fade-up-left" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl overflow-hidden">
+            <div className="flex flex-col-reverse xl:flex-row items-center gap-6">
+              <div className="text-center md:text-left w-full">
                 <h3 className="text-3xl font-semibold mb-2">Question Randomization</h3>
-                <p className="split-text text-xl">Randomize questions to reduce the chances of collaboration and ensure fairness.</p>
+                <p className="split-text text-xl leading-tight">Randomize questions to reduce the chances of collaboration and ensure fairness.</p>
               </div>
-              <Image src="/image/Online_testing/question randmization.webp" alt="Question Randomization" width={200}
-                height={150} className="w-full md:w-1/2 max-w-xs rounded-4xl object-contain" />
+              <Image src="/image/Online_testing/question randmization.webp" alt="Question Randomization" width={200} height={150}
+                className="w-full md:w-1/2 max-w-xs rounded-4xl object-contain" />
             </div>
           </div>
 
           {/* Card 7 */}
-          <div data-aos="fade-up-right" data-aos-duration="2000"
-            className="bg-[#fffff] dark:text-white text-black p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl">
-            <div className="flex flex-col md:flex-row items-center gap-6">
+          <div data-aos="fade-up-right" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-lg hover:shadow-2xl overflow-hidden">
+            <div className="flex flex-col xl:flex-row items-center gap-6">
               <Image src="/image/Online_testing/auto-submitted.webp" alt="Auto Submission" width={200} height={150}
                 className="w-full md:w-1/2 max-w-xs rounded-4xl object-contain" />
-              <div>
+              <div className="text-center md:text-left w-full">
                 <h3 className="text-3xl font-semibold mb-2">Auto Submission</h3>
-                <p className="split-text text-xl">Automatically submit tests when time expires or user completes the test.</p>
+                <p className="split-text text-xl leading-tight">Automatically submit tests when time expires or user completes the test.</p>
               </div>
             </div>
           </div>
@@ -168,7 +159,7 @@ export default function ONLINE_TESTING() {
 
 
 
-        <section data-aos="fade-up" data-aos-duration="2000"  className="text-center dark:text-white  text-black mt-20">
+        <section data-aos="fade-up" data-aos-duration="2000" className="text-center dark:text-white  text-black mt-20">
           <h2 className="text-3xl font-bold mb-4">Ready to try it out?</h2>
           <p className="mb-6 text-2xl  text-gray-700 dark:text-gray-400">Over 20k+ users on ScholarRank. Join them.</p>
           <div className="flex flex-wrap justify-center cursor-pointer gap-4">
--- aiclone/src/app/(products)/procut/page.tsx
+++ aiclone/src/app/(products)/procut/page.tsx
@@ -34,22 +34,11 @@ export default function Classes() {
   }, []);
   return (
     <>
-      <div className="px-4 md:px-20 lg:px-32 py-10 bg-black mx-auto max-w-[96rem] ">
-        <h1 data-aos="fade-right" data-aos-duration="2000" className="text-4xl md:text-5xl text-black dark-text-white text-center font-bold mb-15">PROCUT</h1>
-        <section data-aos="fade-down" data-aos-duration="3000" data-aos-delay="100" className="text-center text-white mb-12 ">
-          <div className="flex flex-row md:flex-row items-center mb-3  ">
-            <div className=" text-2xl text-blue-500"><a href="/">Products</a></div>
-            <div> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
-              <path
-                d="M9.00005 6C9.00005 6 15 10.4189 15 12C15 13.5812 9 18 9 18"
-                stroke="currentColor"
-                strokeWidth="1.5"
-              />
-            </svg>
-            </div>
-            <div className="text-2xl text-green-500">Procut</div>
-          </div>
-          <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-8">
+      <div className="px-4 md:px-20 lg:px-32 py-10  mx-auto max-w-[96rem] ">
+        <h1 data-aos="fade-right" data-aos-duration="2000" className="text-4xl md:text-5xl dark:text-white  text-black text-center font-bold mb-15">PROCUT</h1>
+        <section data-aos="fade-down" data-aos-duration="3000" data-aos-delay="100" className="text-center dark:text-white  text-black  mb-12 ">
+         
+          <div className="flex flex-col-reverse md:flex-row mt-0 items-center justify-between gap-8">
             <div className="md:w-1/2 space-y-20 text-left">
               <h2 className="text-2xl md:text-3xl font-semibold mb-4">Procut: Effortlessly Create Memes, Jokes, and Marketing Videos</h2>
               <p className="split-text mb-6 text-xl">With Procut, you can easily generate engaging content, including memes, jokes, and marketing videos, in just a few clicks. Whether you're looking to entertain your audience or promote your brand, Procut streamlines the creation process, saving you time and effort.</p>
@@ -63,86 +52,100 @@ export default function Classes() {
               alt="procut"
               width={600}
               height={400}
-              className="w-full md:w-1/2 h-auto rounded-4xl object-contain"
+              className="w-full md:w-1/2 hidden md:block rounded-4xl object-contain"
             />
           </div>
         </section>
 
         <section data-aos="zoom-in" data-aos-duration="2000" className="text-center mb-16">
-          <h2 className=" split-text text-3xl text-white font-bold mb-2">The only AI tool you need!</h2>
-          <p className=" split-text text-2xl text-gray-400">Ask anything, generate questions using AI, understand your documents easily, and much more.</p>
+          <h2 className=" split-text text-3xl text-black dark:text-white font-bold mb-2">The only AI tool you need!</h2>
+          <p className=" split-text text-2xl text-gray-700 dark:text-gray-400">Ask anything, generate questions using AI, understand your documents easily, and much more.</p>
         </section>
 
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 text-white">
 
-          <div data-aos="fade-up-right" data-aos-duration="2000"
-            className="bg-[#1f1f1f] p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-2xl">
-            <div className="flex flex-col md:flex-row items-center gap-6">
-              <Image src="/image/procut/meme generate.webp" alt="meme" width={200} height={150}
-                className="w-full md:w-1/2 max-w-xs rounded-4xl object-cover" />
-              <div>
+          {/* Card 1 */}
+          <div data-aos="fade-up-right" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-2xl w-full overflow-hidden">
+            <div className="flex flex-col xl:flex-row items-center gap-6 w-full">
+              <div className="w-full md:w-1/3 flex justify-center">
+                <Image src="/image/procut/meme generate.webp" alt="meme" width={200} height={150}
+                  className="w-[150px] h-auto object-cover rounded-4xl" />
+              </div>
+              <div className="w-full md:w-2/3 text-center md:text-left">
                 <h3 className="text-3xl font-semibold mb-2">Meme Generation</h3>
                 <p className="split-text text-xl">Start by swiftly creating humorous memes that entertain your audience and drive engagement effortlessly.</p>
               </div>
             </div>
           </div>
 
-
-          <div data-aos="fade-up-left" data-aos-delay="50" data-aos-duration="2000"
-            className="bg-[#1f1f1f] p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-2xl">
-            <div className="flex flex-col-reverse md:flex-row items-center gap-6">
-              <div>
+          {/* Card 2 */}
+          <div data-aos="fade-up-left" data-aos-delay="50" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-2xl w-full overflow-hidden">
+            <div className="flex flex-col-reverse xl:flex-row items-center gap-6 w-full">
+              <div className="w-full md:w-2/3 text-center md:text-left">
                 <h3 className="text-3xl font-semibold mb-2">Marketing Video Creation</h3>
                 <p className="split-text text-xl">Design captivating marketing videos to effectively promote your products or services, enhancing your brand's visibility and reach.</p>
               </div>
-              <Image src="/image/procut/marketing video.webp" alt="marketing video" width={200} height={150}
-                className="w-full md:w-1/2 max-w-xs rounded-4xl object-cover" />
+              <div className="w-full md:w-1/3 flex justify-center">
+                <Image src="/image/procut/marketing video.webp" alt="marketing video" width={200} height={150}
+                  className="w-[150px] h-auto object-cover rounded-4xl" />
+              </div>
             </div>
           </div>
 
-          <div data-aos="fade-up-right" data-aos-delay="100" data-aos-duration="2000"
-            className="bg-[#1f1f1f] p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-xl">
-            <div className="flex flex-col md:flex-row items-center gap-6">
-              <Image src="/image/procut/user friendly.webp" alt="user friendly" width={200} height={150}
-                className="w-full md:w-1/2 max-w-xs rounded-4xl object-cover" />
-              <div>
+          {/* Card 3 */}
+          <div data-aos="fade-up-right" data-aos-delay="100" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-xl w-full overflow-hidden">
+            <div className="flex flex-col xl:flex-row items-center gap-6 w-full">
+              <div className="w-full md:w-1/3 flex justify-center">
+                <Image src="/image/procut/user friendly.webp" alt="user friendly" width={200} height={150}
+                  className="w-[150px] h-auto object-cover rounded-4xl" />
+              </div>
+              <div className="w-full md:w-2/3 text-center md:text-left">
                 <h3 className="text-3xl font-semibold mb-2">User-Friendly Interface</h3>
                 <p className="split-text text-xl">Benefit from Procut's intuitive interface, simplifying content creation and making the process both easy and enjoyable.</p>
               </div>
             </div>
           </div>
 
-
-          <div data-aos="fade-up-left" data-aos-delay="150" data-aos-duration="2000"
-            className="bg-[#1f1f1f] p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-2xl">
-            <div className="flex flex-col-reverse md:flex-row items-center gap-6">
-              <div>
+          {/* Card 4 */}
+          <div data-aos="fade-up-left" data-aos-delay="150" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-2xl w-full overflow-hidden">
+            <div className="flex flex-col-reverse xl:flex-row items-center gap-6 w-full">
+              <div className="w-full md:w-2/3 text-center md:text-left">
                 <h3 className="text-3xl font-semibold mb-2">Time-Saving</h3>
                 <p className="split-text text-xl">Save valuable time and resources by utilizing Procut to generate high-quality content quickly, allowing you to focus on other essential tasks.</p>
               </div>
-              <Image src="/image/procut/time saving.webp" alt="time saving" width={200} height={150}
-                className="w-full md:w-1/2 max-w-xs rounded-4xl object-cover" />
+              <div className="w-full md:w-1/3 flex justify-center">
+                <Image src="/image/procut/time saving.webp" alt="time saving" width={200} height={150}
+                  className="w-[150px] h-auto object-cover rounded-4xl" />
+              </div>
             </div>
           </div>
 
-
-          <div data-aos="fade-up-right" data-aos-delay="200" data-aos-duration="2000"
-            className="bg-[#1f1f1f] p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-2xl">
-            <div className="flex flex-col md:flex-row items-center gap-6">
-              <Image src="/image/procut/innovative solution.webp" alt="innovative solution" width={200} height={150}
-                className="w-full md:w-1/2 max-w-xs rounded-4xl object-cover" />
-              <div>
+          {/* Card 5 */}
+          <div data-aos="fade-up-right" data-aos-delay="200" data-aos-duration="1000"
+            className="bg-white dark:bg-gray-900 text-black dark:text-white p-6 rounded-4xl transition-transform transform hover:scale-105 shadow-md hover:shadow-2xl w-full overflow-hidden">
+            <div className="flex flex-col xl:flex-row items-center gap-6 w-full">
+              <div className="w-full md:w-1/3 flex justify-center">
+                <Image src="/image/procut/innovative solution.webp" alt="innovative solution" width={200} height={150}
+                  className="w-[150px] h-auto object-cover rounded-4xl" />
+              </div>
+              <div className="w-full md:w-2/3 text-center md:text-left">
                 <h3 className="text-3xl font-semibold mb-2">Innovative Solution</h3>
                 <p className="split-text text-xl">Embrace Procut as an innovative solution for content creation, empowering you to differentiate yourself in a competitive digital landscape and capture audience attention effectively.</p>
               </div>
             </div>
           </div>
+
         </div>
 
+
         <section data-aos="fade-up" data-aos-duration="2000" className="text-center text-white mt-20">
-          <h2 className="text-3xl font-bold mb-4">Ready to try it out?</h2>
-          <p className="mb-6">Over 20k+ users on ScholarRank. Join them.</p>
+          <h2 className="text-3xl font-bold text-black dark:text-white mb-4">Ready to try it out?</h2>
+          <p className="mb-6 text-gray-700 dark:text-gray-400">Over 20k+ users on ScholarRank. Join them.</p>
           <div className="flex flex-wrap justify-center cursor-pointer gap-4">
             <button className="bg-blue-600 cursor-pointer hover:bg-blue-700 rounded-4xl hover:font-bold text-white font-semibold py-2 px-4 ">Get Started Free</button>
             <button className="bg-white border cursor-pointer border-blue-600 rounded-4xl hover:font-bold text-blue-600 hover:bg-blue-50 font-semibold py-2 px-4 ">View Pricing</button>
